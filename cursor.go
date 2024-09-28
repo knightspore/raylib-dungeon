a88ed22a1bb533a6f17941383c73f6110f370f47
Ciaran <cslemon96@gmail.com>
2024-09-28 18:02:05 +0200

    feat(cursor.go): move cursor to separate module

---
+++ cursor.go
@@ -0,0 +1,84 @@
+package main
+
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+const (
+	texture_color = "textures/cursor_tilesheet.png"
+	texure_normal = "textures/cursor_tilesheet_n.png"
+	animation_fps = 4
+)
+
+type Cursor struct {
+	source       rl.Rectangle
+	dest         rl.Rectangle
+	origin       rl.Vector2
+	rotation     float32
+	textures     map[string]rl.Texture2D
+	frameTimer   float32
+	currentFrame int
+}
+
+func NewCursor(baseSize float32) *Cursor {
+	return &Cursor{
+		source:   rl.NewRectangle(0, 0, 0, 0),
+		dest:     rl.NewRectangle(0, 0, baseSize, baseSize),
+		origin:   rl.NewVector2(baseSize/2, baseSize/2),
+		textures: make(map[string]rl.Texture2D),
+	}
+}
+
+func (c *Cursor) Setup() {
+	c.textures["color"] = rl.LoadTexture(texture_color)
+	c.textures["normal"] = rl.LoadTexture(texure_normal)
+
+	c.source.Width = float32(c.textures["color"].Width / animation_fps)
+	c.source.Height = float32(c.textures["color"].Height)
+}
+
+func (c *Cursor) Cleanup() {
+	rl.UnloadTexture(c.textures["color"])
+	rl.UnloadTexture(c.textures["normal"])
+}
+
+func (c *Cursor) updateAnimation() {
+	c.frameTimer += rl.GetFrameTime()
+	if c.frameTimer >= (1.0 / float32(animation_fps)) {
+		c.currentFrame = (c.currentFrame + 1) % animation_fps
+		c.frameTimer = 0
+	}
+	c.source.X = float32(c.currentFrame * int(c.textures["color"].Width))
+}
+
+func (c *Cursor) updatePosition() {
+	halfSize := float32(c.dest.Height / 2)
+
+	dx, dy, dw, dh :=
+		float32(rl.GetMousePosition().X-halfSize),
+		float32(rl.GetMousePosition().Y-halfSize),
+		float32(c.dest.Width),
+		float32(c.dest.Height)
+
+	c.rotation = float32(rl.GetTime() * animation_fps * 10)
+	c.dest = rl.NewRectangle(dx, dy, dw, dh)
+}
+
+func (c *Cursor) Update() {
+	c.updateAnimation()
+	c.updatePosition()
+}
+
+func (c *Cursor) Draw() {
+	rl.DrawTexturePro(c.textures["color"], c.source, c.dest, c.origin, c.rotation, rl.White)
+
+	if DEBUG {
+		rl.DrawCircle(int32(c.center().X), int32(c.center().Y), 5, rl.Red)
+	}
+}
+
+func (c *Cursor) DrawNormal() {
+	rl.DrawTexturePro(c.textures["normal"], c.source, c.dest, c.origin, c.rotation, rl.White)
+}
+
+func (c *Cursor) center() rl.Vector2 {
+	return rl.NewVector2(c.dest.X, c.dest.Y)
+}
