30ffa80c27baf19555a99eb441ffc9ec410e3904
Ciaran <cslemon96@gmail.com>
2024-09-28 16:30:30 +0200

    refactor(postprocess.fs): Update to fix the alpha bleed issues

--- shaders/postprocess.fs
+++ shaders/postprocess.fs
@@ -5,9 +5,9 @@ in vec4 fragColor;
 out vec4 finalColor;
 uniform sampler2D tex;
 
-uniform int size = 1; // blur size
+uniform int size = 2; // blur size
 uniform float separation = 3.0; // spread
-uniform float threshold = 0.9;
+uniform float threshold = 0.3;
 uniform float amount = 1.0;
 
 uniform float chromaticAberration = 0.005;
@@ -15,38 +15,38 @@ uniform float chromaticAberration = 0.005;
 void main() {
     vec2 texSize = textureSize(tex, 0).xy;
     float dist = length(fragTexCoord.y - 0.5);
-    
+
     vec4 resultR = vec4(0.0);
     vec4 resultG = vec4(0.0);
     vec4 resultB = vec4(0.0);
     float count = 0.0;
-    
+
     for (int x = -size; x <= size; x++) {
         for (int y = -size; y <= size; y++) {
             vec2 offset = (vec2(x, y) * separation * dist) / texSize;
-            
+
             // Sample each color channel with a slight offset
-            vec4 colorR = texture(tex, fragTexCoord + offset + vec2(chromaticAberration, 0.0) * dist * 2.0);
-            vec4 colorG = texture(tex, fragTexCoord + offset + vec2(0.0, chromaticAberration) * dist * 2.0);
+            vec4 colorR = texture(tex, fragTexCoord + offset + vec2(chromaticAberration, 0.0) * dist * separation);
+            vec4 colorG = texture(tex, fragTexCoord + offset);
             vec4 colorB = texture(tex, fragTexCoord + offset - vec2(chromaticAberration, 0.0) * dist * 2.0);
-            
+
             // Apply threshold
             float valueR = max(colorR.r, max(colorR.g, colorR.b));
             float valueG = max(colorG.r, max(colorG.g, colorG.b));
             float valueB = max(colorB.r, max(colorB.g, colorB.b));
-            
-            if (valueR > threshold) colorR = vec4(0.0);
-            if (valueG > threshold) colorG = vec4(0.0);
-            if (valueB > threshold) colorB = vec4(0.0);
-            
+
+            if (valueR > threshold) colorR += colorR * (valueR - threshold);
+            if (valueG > threshold) colorG += colorG * (valueG - threshold);
+            if (valueB > threshold) colorB += colorB * (valueB - threshold);
+
             resultR += colorR;
             resultG += colorG;
             resultB += colorB;
             count++;
         }
     }
-    
+
     vec4 result = vec4(resultR.r, resultG.g, resultB.b, (resultR.a + resultG.a + resultB.a) / 3.0) / count;
-    
+
     finalColor = fragColor * mix(vec4(1.0), result, amount);
 }
